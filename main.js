906ca7f9edb27750d3b5e44235f6fbda97b4759d
Boogeyman14 <abannepal14@gmail.com>
2021-04-13 17:24:05 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+fetch("https://nepalcorona.info/api/v1/data/nepal")
+  .then((apiData) => {
+    return apiData.json();
+  })
+  .then((data) => {
+    const totalCases = data.tested_total;
+    const totalRecovered = data.recovered;
+    const totalDeaths = data.deaths;
+
+    const isolated = data.in_isolation;
+    const quarantine = data.quarantined;
+    const pending = data.pending_result;
+
+    const testedPos = data.tested_positive;
+    const testedNeg = data.tested_negative;
+    const testedRdt = data.tested_rdt;
+
+    const update = data.updated_at;
+
+    document.getElementById("cases").innerHTML = `${totalCases}`;
+    document.getElementById("recover").innerHTML = `${totalRecovered}`;
+    document.getElementById("death").innerHTML = `${totalDeaths}`;
+
+    document.getElementById("isolate").innerHTML = `${isolated}`;
+    document.getElementById("quarantine").innerHTML = `${quarantine}`;
+    document.getElementById("pending").innerHTML = `${pending}`;
+
+    document.getElementById("tested_positive").innerHTML = `${testedPos}`;
+    document.getElementById("tested_negative").innerHTML = `${testedNeg}`;
+    document.getElementById("tested_rdt").innerHTML = `${testedRdt}`;
+
+    document.getElementById("updated").innerHTML = `Last updated: ${update}`;
+  })
+  .catch(() => {
+    document.querySelector(".error-msg").style = "display:block !important";
+  });
